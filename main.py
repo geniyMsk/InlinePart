75942b63c8d684ed2cb74f3195446b2f448a0f41
geniyMsk <83307950+geniyMsk@users.noreply.github.com>
2021-05-15 22:32:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ inline_keyboard = InlineKeyboardMarkup(
         ]
 
 ])
-pressed = []
+
 
 db=DBCommands()
 
@@ -32,15 +32,16 @@ db=DBCommands()
 async def start(message: Message):
     await message.answer(text='Какой-то текст', reply_markup=inline_keyboard)
     chatid = message.from_user.id
-    message_id = message.message_id+1
-    await db.create()
+    message_id = message.message_id+1#СООБЩЕНИЕ С КЛАВИАТУРОЙ
+
+    await db.create()#СОЗДАЕМ БД
     try:
-        await db.add_user((chatid,))
+        await db.add_user((chatid,))#ДОБАВЛЯЕМ ПОЛЬЗОВАТЕЛЯ ЕСЛИ ЕГО НЕТ В БД
     except:
         pass
     try:
-        await db.update_buttons(par=('[]', chatid))
-        await db.update_message_id(par = (message_id, chatid))
+        await db.update_buttons(par=('[]', chatid))#ОБНУЛЯЕМ ПРЕДЫДУЩИЕ СПИСКИ НАЖАТЫХ КНОПОК
+        await db.update_message_id(par = (message_id, chatid))#ЗАПИСЫВАЕМ ID СООБЩЕНИЯ
     except:
         pass
     await states.on.set()
@@ -51,40 +52,43 @@ async def start(message: Message):
 async def inline(query: CallbackQuery):
 
     chatid = query.from_user.id
-    message_id = query.message.message_id
-    i = 1
     keyboard = query.message.reply_markup
+    pressed_buttons = json.loads((await db.select_buttons(chatid=(chatid,))).fetchone()[0])
+
+    i = 1
+    message_id = query.message.message_id
     message_id_db = (await db.select_message_id(chatid=(chatid,))).fetchone()[0]
-    pressed = json.loads((await db.select_buttons(chatid=(chatid,))).fetchone()[0])
-    if message_id_db==message_id:
-        while i<=5:
+    if message_id_db==message_id:#БУДЕТ РАБОТАТЬ ТОЛЬКО НА ПОСЛЕДНЕЙ КЛАВИАТУРЕ
+        while i<=5:#ПЕРЕБОР ВСЕХ КНОПОК
             if query.data==f'{i}':
-                if i<=3:
+                if i<=3:#ДЛЯ ПЕРВОГО РЯДА КНОПОК
                     text_button = keyboard.inline_keyboard[0][i-1]['text']
 
-                    if text_button == f'{i}':
+                    if text_button == f'{i}':#ЕСЛИ КНОПКА НЕ БЫЛА НАЖАТА
                         keyboard.inline_keyboard[0][i - 1]['text'] = emojize(f'{i}:check_mark_button:')
-                        pressed.append(i)
-                        await bot.edit_message_reply_markup(chat_id=query.from_user.id, message_id=message_id, reply_markup=keyboard)
-                    else:
-                        pressed.remove(i)
-                        keyboard.inline_keyboard[0][i - 1]['text'] = emojize(f'{i}')
-                        await bot.edit_message_reply_markup(chat_id=query.from_user.id, message_id=message_id,reply_markup=keyboard)
-
-                else:
-                    text_button = keyboard.inline_keyboard[1][i - 4]['text']
-                    if text_button == f'{i}':
-                        keyboard.inline_keyboard[1][i - 4]['text'] = emojize(f'{i}:check_mark_button:')
-                        pressed.append(i)
+                        pressed_buttons.append(i)
                         await bot.edit_message_reply_markup(chat_id=query.from_user.id, message_id=message_id,
                                                             reply_markup=keyboard)
-                    else:
+                    else:#ЕСЛИ КНОПКА БЫЛА НАЖАТА
+                        pressed_buttons.remove(i)
+                        keyboard.inline_keyboard[0][i - 1]['text'] = emojize(f'{i}')
+                        await bot.edit_message_reply_markup(chat_id=query.from_user.id, message_id=message_id,
+                                                            reply_markup=keyboard)
+
+                else:#ДЛЯ ВТОРОГО РЯДА КНОПОК
+                    text_button = keyboard.inline_keyboard[1][i - 4]['text']
+                    if text_button == f'{i}':#ЕСЛИ КНОПКА НЕ БЫЛА НАЖАТА
+                        keyboard.inline_keyboard[1][i - 4]['text'] = emojize(f'{i}:check_mark_button:')
+                        pressed_buttons.append(i)
+                        await bot.edit_message_reply_markup(chat_id=query.from_user.id, message_id=message_id,
+                                                            reply_markup=keyboard)
+                    else:#ЕСЛИ КНОПКА БЫЛА НАЖАТА
                         keyboard.inline_keyboard[1][i - 4]['text'] = emojize(f'{i}')
-                        pressed.remove(i)
+                        pressed_buttons.remove(i)
                         await bot.edit_message_reply_markup(chat_id=query.from_user.id, message_id=message_id,reply_markup=keyboard)
             i+=1
         try:
-            await db.update_buttons(par=(f'{pressed}', chatid))
+            await db.update_buttons(par=(f'{pressed_buttons}', chatid))#ЗАПИСЫВАЕМ В БД НАЖАТЫЕ КНОПКИ
         except:
             pass
 
@@ -93,12 +97,13 @@ async def inline(query: CallbackQuery):
 async def button(message: types.Message):
     chatid = message.from_user.id
 
-    buttons = json.loads((await db.select_buttons(chatid=(chatid,))).fetchone()[0])
+    pressed_buttons = json.loads((await db.select_buttons(chatid=(chatid,))).fetchone()[0])#СПИСОК НАЖАТЫХ КНОПОК ИЗ БД
     text=''
-    for button in sorted(buttons):
+    for button in sorted(pressed_buttons):
         text += f'{button}, '
-    x=len(text)-2
+    x=len(text)-2#ЧТОБЫ В КОНЦЕ НЕ БЫЛО ЗАПЯТОЙ И ПРОБЕЛА
     await message.answer(text='У вас нажаты кнопки: '+text[:x])
+
 @dp.message_handler(commands=['stop'], state='*')
 async def stop(message: types.Message):
     await message.answer("Нельзя изменять символ на кнопках")
