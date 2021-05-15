3ad4ceeeed1fe1187ea50940e64567e190052bd8
geniyMsk <83307950+geniyMsk@users.noreply.github.com>
2021-05-15 22:00:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from aiogram import types
 from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
+from DBCommands import DBCommands
+
 class states(StatesGroup):
     on = State()
     off=State()
@@ -24,66 +26,79 @@ inline_keyboard = InlineKeyboardMarkup(
 ])
 pressed = []
 
+db=DBCommands()
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=['start'], state='*')
 async def start(message: Message):
     await message.answer(text='Какой-то текст', reply_markup=inline_keyboard)
+    chatid = message.from_user.id
+    message_id = message.message_id+1
+    await db.create()
+    try:
+        await db.add_user((chatid,))
+    except:
+        pass
+    try:
+        await db.update_buttons(par=('[]', chatid))
+        await db.update_message_id(par = (message_id, chatid))
+    except:
+        pass
     await states.on.set()
+
 
 
 @dp.callback_query_handler(state=states.on)
 async def inline(query: CallbackQuery):
-    i = 1
-    message_id = query.message.message_id
+
     chatid = query.from_user.id
-    keyboard=query.message.reply_markup
-    while i<=5:
-        if query.data==f'{i}':
-            if i<=3:
-                text_button = keyboard.inline_keyboard[0][i-1]['text']
+    message_id = query.message.message_id
+    i = 1
+    keyboard = query.message.reply_markup
+    message_id_db = (await db.select_message_id(chatid=(chatid,))).fetchone()[0]
+    pressed = json.loads((await db.select_buttons(chatid=(chatid,))).fetchone()[0])
+    if message_id_db==message_id:
+        while i<=5:
+            if query.data==f'{i}':
+                if i<=3:
+                    text_button = keyboard.inline_keyboard[0][i-1]['text']
 
-                if text_button == f'{i}':
-                    keyboard.inline_keyboard[0][i - 1]['text'] = emojize(f'{i}:check_mark_button:')
-                    pressed.append(i)
-                    await bot.edit_message_reply_markup(chat_id=query.from_user.id, message_id=message_id, reply_markup=keyboard)
-                else:
-                    pressed.remove(i)
-                    keyboard.inline_keyboard[0][i - 1]['text'] = emojize(f'{i}')
-                    await bot.edit_message_reply_markup(chat_id=query.from_user.id, message_id=message_id,reply_markup=keyboard)
+                    if text_button == f'{i}':
+                        keyboard.inline_keyboard[0][i - 1]['text'] = emojize(f'{i}:check_mark_button:')
+                        pressed.append(i)
+                        await bot.edit_message_reply_markup(chat_id=query.from_user.id, message_id=message_id, reply_markup=keyboard)
+                    else:
+                        pressed.remove(i)
+                        keyboard.inline_keyboard[0][i - 1]['text'] = emojize(f'{i}')
+                        await bot.edit_message_reply_markup(chat_id=query.from_user.id, message_id=message_id,reply_markup=keyboard)
 
-            else:
-                text_button = keyboard.inline_keyboard[1][i - 4]['text']
-                if text_button == f'{i}':
-                    keyboard.inline_keyboard[1][i - 4]['text'] = emojize(f'{i}:check_mark_button:')
-                    pressed.append(i)
-                    await bot.edit_message_reply_markup(chat_id=query.from_user.id, message_id=message_id,
-                                                        reply_markup=keyboard)
                 else:
-                    keyboard.inline_keyboard[1][i - 4]['text'] = emojize(f'{i}')
-                    pressed.remove(i)
-                    await bot.edit_message_reply_markup(chat_id=query.from_user.id, message_id=message_id,reply_markup=keyboard)
-        i+=1
-    with sqlite3.connect('server.db') as db:
-        cursor = db.cursor()
-        cursor.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER UNIQUE, buttons TEXT)""")
+                    text_button = keyboard.inline_keyboard[1][i - 4]['text']
+                    if text_button == f'{i}':
+                        keyboard.inline_keyboard[1][i - 4]['text'] = emojize(f'{i}:check_mark_button:')
+                        pressed.append(i)
+                        await bot.edit_message_reply_markup(chat_id=query.from_user.id, message_id=message_id,
+                                                            reply_markup=keyboard)
+                    else:
+                        keyboard.inline_keyboard[1][i - 4]['text'] = emojize(f'{i}')
+                        pressed.remove(i)
+                        await bot.edit_message_reply_markup(chat_id=query.from_user.id, message_id=message_id,reply_markup=keyboard)
+            i+=1
         try:
-            cursor.execute(f"""INSERT INTO users (id, buttons) VALUES ({chatid}, '{pressed}')""")
+            await db.update_buttons(par=(f'{pressed}', chatid))
         except:
-            cursor.execute(f"""UPDATE users SET buttons = '{pressed}' WHERE id={chatid}""")
+            pass
+
 
 @dp.message_handler(commands=['button'], state='*')
 async def button(message: types.Message):
     chatid = message.from_user.id
-    with sqlite3.connect('server.db') as db:
-        cursor = db.cursor()
-        buttons = json.loads(cursor.execute(f"""SELECT buttons FROM users WHERE id={chatid}""").fetchone()[0])
-        text=''
-        for button in buttons:
-            text += f'{button}, '
 
-
-        x=len(text)-2
-        await message.answer(text='У вас нажаты кнопки: '+text[:x])
+    buttons = json.loads((await db.select_buttons(chatid=(chatid,))).fetchone()[0])
+    text=''
+    for button in sorted(buttons):
+        text += f'{button}, '
+    x=len(text)-2
+    await message.answer(text='У вас нажаты кнопки: '+text[:x])
 @dp.message_handler(commands=['stop'], state='*')
 async def stop(message: types.Message):
     await message.answer("Нельзя изменять символ на кнопках")
